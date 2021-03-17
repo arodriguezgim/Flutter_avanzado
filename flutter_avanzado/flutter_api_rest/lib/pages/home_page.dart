559f6117cb17aac77b7ef7ec16a810857d346755
arodriguezgim <71493718+arodriguezgim@users.noreply.github.com>
2021-03-17 11:46:58 +0100

    mediaqueries

--- flutter_avanzado/flutter_api_rest/lib/pages/home_page.dart
+++ flutter_avanzado/flutter_api_rest/lib/pages/home_page.dart
@@ -9,6 +9,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final double pinkSize = size.width * 0.8;
+    final double orangeSize = size.width * 0.57;
+
     return Scaffold(
       body: Container(
         width: double.infinity,
@@ -17,17 +21,17 @@ class _HomePageState extends State<HomePage> {
         child: Stack(
           children: [
             Positioned(
-                top: -140,
-                right: -50,
+                top: -pinkSize * 0.4,
+                right: -pinkSize * 0.2,
                 child: Circle(
-                  size: 340,
+                  size: pinkSize,
                   colors: [Colors.pinkAccent, Colors.pink],
                 )),
             Positioned(
-                top: -100,
-                left: -50,
+                top: -orangeSize * 0.55,
+                left: -orangeSize * 0.15,
                 child: Circle(
-                  size: 240,
+                  size: orangeSize,
                   colors: [Colors.orange, Colors.deepOrangeAccent],
                 )),
           ],
